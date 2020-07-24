5bc30b12cfdec8deab3624434d25f02641c1dced
Shubham Yadav <shubham.relieved@gmail.com>
2020-07-24 23:02:09 +0530

    data and template seperation

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,31 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Express',
+    name: 'Shubham',
+    posts: [{
+      title: "this is post title",
+      body: "As your wish post ",
+      cover: "https://picsum.photos/200/100"
+    },
+    {
+      title: "this is post title 2",
+      body: "As your wish post 2 ",
+      cover: "https://picsum.photos/200/100",
+
+    },
+    {
+      title: "this is post title 3",
+      body: "As your wish post 3 ",
+      cover: "https://picsum.photos/200/100",
+
+    }
+    ]
+
+  });
 });
 
 module.exports = router;
