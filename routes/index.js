80b20557caab6a1f1d48ee842ddc7c7c1bf0f7c9
Shubham Yadav <shubham.relieved@gmail.com>
2020-07-25 13:44:59 +0530

    in memory form data store

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var posts = [{
+  title: "this is post title",
+  body: "As your wish post ",
+  cover: "https://picsum.photos/200/100"
+},
+{
+  title: "this is post title 2",
+  body: "As your wish post 2 ",
+  cover: "https://picsum.photos/200/100",
+
+},
+
+]
 
 /* GET home page. */
 
@@ -7,26 +20,28 @@ router.get('/', function (req, res, next) {
   res.render('index', {
     title: 'Express',
     name: 'Shubham',
-    posts: [{
-      title: "this is post title",
-      body: "As your wish post ",
-      cover: "https://picsum.photos/200/100"
-    },
-    {
-      title: "this is post title 2",
-      body: "As your wish post 2 ",
-      cover: "https://picsum.photos/200/100",
-
-    },
-    {
-      title: "this is post title 3",
-      body: "As your wish post 3 ",
-      cover: "https://picsum.photos/200/100",
-
-    }
-    ]
-
+    posts: posts
   });
+
+
+
+
 });
+
+router.post('/create', function (req, res, next) {
+
+
+  const title = req.body.title;
+  const body = req.body.body;
+  const post = {
+    title: title,
+    body: body,
+    cover: "https://picsum.photos/200/100",
+
+  }
+  posts.push(post)
+
+  res.redirect('/')
+})
 
 module.exports = router;
